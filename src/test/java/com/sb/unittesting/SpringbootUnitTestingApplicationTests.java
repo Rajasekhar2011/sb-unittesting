36c509aa349416b8b97fb0def62ab49fe72510cf
RajasekharReddy.Kolli <rajasekharreddy.kolli@cognizant.com>
2021-06-26 20:19:31 +0530

    firstcommit

---
+++ src/test/java/com/sb/unittesting/SpringbootUnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.unittesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
