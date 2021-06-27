a6617ca7222aa75fdb9820159166e1766606a84e
RajasekharReddy.Kolli <rajasekharreddy.kolli@cognizant.com>
2021-06-27 10:18:39 +0530

    using mockito annotations

--- src/test/java/com/sb/unittesting/mockito/ServiceImplTestWithMockito.java
+++ src/test/java/com/sb/unittesting/mockito/ServiceImplTestWithMockito.java
@@ -7,15 +7,28 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import com.sb.unittesting.repoistory.SomeDataService;
 import com.sb.unittesting.service.ServiceImpl;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceImplTestWithMockito {
 
-	ServiceImpl si = new ServiceImpl();
-	//mock method to mock the interfaces so that it creates instance of it
-	SomeDataService sds = mock(SomeDataService.class);
+	/*
+	 * ServiceImpl si = new ServiceImpl();
+	 *  //mock method to mock the interfaces so that it creates instance of it 
+	 * SomeDataService sds = mock(SomeDataService.class);
+	 */
+	
+	@Mock
+	SomeDataService sds;
+	
+	@InjectMocks
+	ServiceImpl si;
 
 	@Test
 	public void test_calculateSumUsingMockito() {
